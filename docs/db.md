475e9b025ed2e0741d8238d711b6bd058e35f9b3
Kuba <339630@uwr.edu.pl>
2023-12-10 09:51:47 +0100

    Basic skeleton of urls for create/edit routes

---
+++ docs/db.md
@@ -0,0 +1,9 @@
+Route is:
+* id
+* name
+* owner
+* list of waypoints:
+  * coordinates
+  * name
+  * description
+* link for joining
--- src/index.js
+++ src/index.js
@@ -12,6 +12,24 @@ app.use(urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.render('index');
 })
+app.get('/admin/routes/list', (req, res) => {
+    // TODO list all routes
+    res.render('admin-list');
+})
+app.get('/admin/routes/create', (req, res) => {
+    // TODO: create a new route with id
+    const newId = 42;
+    res.redirect(`/admin/routes/edit/${newId}`);
+})
+app.get('/admin/routes/edit/:id', (req, res) => {
+    const id = req.params.id;
+    res.render('routes-edit', { id: id });
+})
+app.post('/admin/routes/edit/:id', (req, res) => {
+    const id = req.params.id;
+    // TODO update the changes to the database
+    res.redirect('/admin/routes/list');
+})
 
 createServer(app).listen(3000);
 
