74c7d4c2a95ecdb419f9911964f5e91b0ffc4576
Kuba <339630@uwr.edu.pl>
2023-12-30 13:34:27 +0100

    create/edit route & waypoint validation

--- src/index.js
+++ src/index.js
@@ -371,19 +371,65 @@ function getWaypointsFromRequest (req) {
     }
     return waypoints;
 }
+
+/**
+ * Returns null if okay otherwise an object for response
+ * @param {string} routeName 
+ * @param {array} waypoints 
+ */
+function validateRoute (route, waypoints) {
+    let message = null;
+    if (route.name === '') {
+        message = `Route name cannot be empty!`;
+    } 
+    for (const waypoint of waypoints) {
+        if (waypoint.name === '') {
+            message = `Waypoint name cannot be empty! (Waypoint ${waypoint.id})`;
+            break;
+        }
+
+        if (waypoint.lat === '' || waypoint.lng === '') {
+            message = `Waypoint coordinates cannot be empty! (Waypoint ${waypoint.id})`;
+            break;   
+        }
+    }
+    if (message !== null) {
+        const waypointsResponse = waypoints.map((w, _) => {
+            return {
+                order_id: w.id,
+                latitude: w.lat,
+                longitude: w.lng,
+                name: w.name,
+            }
+        });
+
+        return {
+            message: message,
+            route: route,
+            waypoints: waypointsResponse
+        }
+    }
+
+    return null;
+
+}
 app.post('/admin/routes/create', authorize, async (req, res) => {
     const userId = req.user;
-    const name = req.body.name;
+    const routeName = req.body.name;
     const waypoints = getWaypointsFromRequest(req);
 
-    // TODO: check if the user is the owner of the route
-    // TOOD: check if the route exists
-    // TODO: check if waypoints are correct (lat, lng, ids)
+    const validation = validateRoute({ name: routeName }, waypoints);
+    if (validation !== null) {
+        res.render('routes-edit', {
+            message: validation.message,
+            mode: CREATE_ROUTE,
+            route: validation.route,
+            waypoints: validation.waypoints
+        });
+        return;
+    }
 
-    console.log(name, userId);
-    console.log(waypoints);
-
-    await createRoute(name, userId, waypoints);
+    await createRoute(routeName, userId, waypoints);
     res.redirect('/admin/routes/list');
 })
 app.get('/admin/routes/edit/:route_id', [authorize, injectUser], async (req, res) => {
@@ -395,6 +441,16 @@ app.get('/admin/routes/edit/:route_id', [authorize, injectUser], async (req, res
         });
         return;
     }
+    const userId = req.signedCookies.user;
+    const ownerId = route.owner_id;
+    // TODO: is this comparison with .toString() correct?
+    if (ownerId.toString() !== userId) {
+        res.render('error-generic', {
+            message: 'You are not the owner of this route!'
+        });
+        return;
+    }
+
     const waypoints = await getWaypoints(routeId);
     res.render('routes-edit', { 
         mode: EDIT_ROUTE, 
@@ -404,18 +460,43 @@ app.get('/admin/routes/edit/:route_id', [authorize, injectUser], async (req, res
 })
 app.post('/admin/routes/edit', authorize,  async (req, res) => {
     const routeId = req.body.route_id;
-    const name = req.body.name;
+    const routeName = req.body.name;
     const userId = req.user;
     const waypoints = getWaypointsFromRequest(req);
-    
-    // console.log(route_id, name, owner_id);
-    // console.log(waypoints);
 
-    // TODO: check if the user is the owner of the route
-    // TOOD: check if the route exists
-    // TODO: check if waypoints are correct (lat, lng, ids)
+    const route = await getRoute(routeId); 
+    // check if the route exists
+    if (!route) {
+        console.log(`routeId=${routeId}`);
+        console.log(`route=${route}`);
+        res.render('error-generic', {
+            message: 'No route with this id!'
+        });
+        return;
+    }
 
-    await updateRoute(routeId, name, userId, waypoints);
+    // check if the user is the owner of the route
+    const ownerId = route.owner_id;
+    if (ownerId.toString() !== userId) {
+        res.render('error-generic', {
+            message: 'You are not the owner of this route!'
+        });
+        return;
+    } 
+
+    // check if route and waypoints are correct
+    const validation = validateRoute(route, waypoints);
+    if (validation !== null) {
+        res.render('routes-edit', {
+            message: validation.message,
+            mode: EDIT_ROUTE,
+            route: validation.route,
+            waypoints: validation.waypoints
+        });
+        return;
+    }
+
+    await updateRoute(routeId, routeName, userId, waypoints);
     res.redirect('/admin/routes/list');
 })
 
