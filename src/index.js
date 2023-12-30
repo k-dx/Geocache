2fdfd6bb41c1b70199c67fb53d42f69e6ee77b36
Kuba <339630@uwr.edu.pl>
2023-12-30 21:09:59 +0100

    move cookie parser secret to env

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.set('views', './src/views');
 app.use(express.static('./src/public'));
 
 app.use(urlencoded({ extended: true }));
-app.use(cookieParser('djdiej4iutjf323xmzz02mfdg'));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.get('/', injectUser, (req, res) => {
     res.render('index');
