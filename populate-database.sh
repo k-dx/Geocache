fa42fa0b6ac75685ba778cc6dad69ccbcff3c35b
k-dx <tornadowro@gmail.com>
2025-06-27 13:57:07 +0200

    .

--- populate-database.sh
+++ populate-database.sh
@@ -4,7 +4,7 @@
 sudo mysql -h 127.0.0.1 -u root -p < /home/kuba/Geocache/geocache.session.sql
 
 # create the users (by sending POST requests to the server cuz password hashing)
-for i in {1..10}
+for i in {1..25}
 do
   curl -X POST http://localhost:3000/register -d "username=user$i&email=user$i@example.com&password=password"
   echo "User $i created"
